9230018f450a51ede37a4c16f9d98ec90b6a1301
Aditya Sarma <aditya@wadhwaniai.org>
2020-06-15 14:55:46 +0530

    Remove h36m stuff

--- datasets/mixed_dataset.py
+++ datasets/mixed_dataset.py
@@ -9,19 +9,20 @@ from .base_dataset import BaseDataset
 class MixedDataset(torch.utils.data.Dataset):
 
     def __init__(self, options, **kwargs):
-        self.dataset_list = ['h36m', 'lsp-orig', 'mpii', 'lspet', 'coco', 'mpi-inf-3dhp']
-        self.dataset_dict = {'h36m': 0, 'lsp-orig': 1, 'mpii': 2, 'lspet': 3, 'coco': 4, 'mpi-inf-3dhp': 5}
+        self.dataset_list = ['lsp-orig', 'mpii', 'lspet', 'coco', 'mpi-inf-3dhp']
+        self.dataset_dict = {'lsp-orig': 0, 'mpii': 1, 'lspet': 2, 'coco': 3, 'mpi-inf-3dhp': 4}
         self.datasets = [BaseDataset(options, ds, **kwargs) for ds in self.dataset_list]
         total_length = sum([len(ds) for ds in self.datasets])
         length_itw = sum([len(ds) for ds in self.datasets[1:-1]])
         self.length = max([len(ds) for ds in self.datasets])
+
         """
-        Data distribution inside each batch:
-        30% H36M - 60% ITW - 10% MPI-INF
+            Data distribution inside each batch:
+            30% H36M - 60% ITW - 10% MPI-INF
         """
         self.partition = [.3, .6*len(self.datasets[1])/length_itw,
                           .6*len(self.datasets[2])/length_itw,
-                          .6*len(self.datasets[3])/length_itw, 
+                          .6*len(self.datasets[3])/length_itw,
                           .6*len(self.datasets[4])/length_itw,
                           0.1]
         self.partition = np.array(self.partition).cumsum()
--- preprocess_datasets.py
+++ preprocess_datasets.py
@@ -20,7 +20,7 @@ parser.add_argument('--eval_files', default=False, action='store_true', help='Ex
 
 if __name__ == '__main__':
     args = parser.parse_args()
-    
+
     # define path to store extra files
     out_path = cfg.DATASET_NPZ_PATH
     openpose_path = cfg.OPENPOSE_PATH
@@ -43,12 +43,12 @@ if __name__ == '__main__':
 
     if args.eval_files:
         # Human3.6M preprocessing (two protocols)
-        h36m_extract(cfg.H36M_ROOT, out_path, protocol=1, extract_img=True)
-        h36m_extract(cfg.H36M_ROOT, out_path, protocol=2, extract_img=False)
-        
+        # h36m_extract(cfg.H36M_ROOT, out_path, protocol=1, extract_img=True)
+        # h36m_extract(cfg.H36M_ROOT, out_path, protocol=2, extract_img=False)
+
         # MPI-INF-3DHP dataset preprocessing (test set)
         mpi_inf_3dhp_extract(cfg.MPI_INF_3DHP_ROOT, openpose_path, out_path, 'test')
-        
+
         # 3DPW dataset preprocessing (test set)
         pw3d_extract(cfg.PW3D_ROOT, out_path)
 
